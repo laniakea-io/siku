2a3d51146b6e71f33fb83bed4579b433ffba30b2
Paul <williams.thrive@proton.me>
2026-05-29 16:58:38 -0500

    Added new songs, updated mobile pdf version, fixed typos

---
+++ mobile.js
@@ -0,0 +1,51 @@
+const toml = require("toml");
+const fs = require("fs");
+
+fs.readFile("./repertorio.toml", "utf-8", (err, data) => {
+  if (err) {
+    console.error(err);
+  } else {
+    parsed = toml.parse(data).source;
+    sorted = parsed.sort((a, b) =>
+      a.name.toLowerCase().localeCompare(b.name.toLowerCase()),
+    );
+
+    sorted.forEach((song) => {
+      if (song.img === undefined) {
+        img = "";
+      } else {
+        img = `image::img/${song.img}[]`;
+      }
+      if (song.audio === undefined) {
+        audio = "";
+      } else {
+        audio = `audio::audio/${song.audio}[]`;
+      }
+      if (song.lyrics === undefined) {
+        lyrics = "";
+      } else {
+        lyrics = song.lyrics;
+      }
+      if (song.author === undefined) {
+        author = "";
+      } else {
+        author = `${song.author} | `;
+      }
+      if (song.type === undefined) {
+        type = "";
+      } else {
+        type = `Estilo: ${song.type}`;
+      }
+
+      let template = `
+== ${song.name}\n
+${author}${type}
+[source]
+----
+${song.numerofonia}
+----
+${lyrics}`;
+      console.log(template);
+    });
+  }
+});
--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 bun run ./repertorio.js > ./repertorio.adoc
 
 # pdf mobile version
-cat header-mobile.adoc repertorio.adoc > mobile.adoc # join header & repertorio
+bun run ./mobile.js > ./content.adoc
+cat header-mobile.adoc content.adoc > mobile.adoc # join header & repertorio
 asciidoctor-pdf -r asciidoctor-mathematical -o mobile.pdf mobile.adoc
 
 # web version
